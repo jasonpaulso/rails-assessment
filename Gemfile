32fa5c4fce123e5b589d2a7fb657f1746f47e230
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-06-03 11:18:47 -0700

    implemented devise

--- Gemfile
+++ Gemfile
@@ -27,6 +27,13 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'tod'
 #friendly_id slugifier http://norman.github.io/friendly_id/
 gem 'friendly_id', '~> 5.1.0'
+#devise
+gem 'devise'
+#rubygems
+
+
+
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :user_shows
   has_many :shows, through: :user_shows
   has_many :actors, through: :shows
   has_many :networks, through: :shows
-  
+
 end
