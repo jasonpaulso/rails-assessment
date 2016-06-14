3ca19b8f9a0aacb5a7a4be2c5d7bdf13263d7dda
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-06-14 12:49:39 -0700

    added logic to networks_controller to prevent rendering show view if network is nil

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -1,12 +1,15 @@
 class NetworksController < ApplicationController
   def index
-
     @networks = Network.all
   end
 
-
   def show
-    # binding.pry
     @network = Network.find_by(slug:params[:id])
+    if !@network.nil?
+      @network
+    else
+      flash[:error] = "That network does not exist."
+      redirect_to networks_path
+    end
   end
 end
