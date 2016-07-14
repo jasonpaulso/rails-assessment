c9ed306cb926b2e0f80315255d1749512a76cdf8
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-07-14 15:55:53 -0700

    testing adding images to show index

--- app/serializers/show_serializer.rb
+++ app/serializers/show_serializer.rb
@@ -1,5 +1,5 @@
 class ShowSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description
+  attributes :id, :title, :description, :url
   has_one :network, serializer: ShowNetworkSerializer
   # has_many :users
 end
