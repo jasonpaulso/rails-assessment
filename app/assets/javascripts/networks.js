79dd8be4f8a18204446360b476f4f5f0fa451578
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-07-19 11:51:42 -0700

    moved network related js to networks.js

---
+++ app/assets/javascripts/networks.js
@@ -0,0 +1,30 @@
+$('.networks.index').ready(function() {
+  
+});
+$('.networks.show').ready(function(){
+  loadNetworkShows();
+});
+
+function loadNetworkShows() {
+  event.preventDefault();
+  var networkSlug = $('.networkShows').data("id");
+  $.ajax({
+    type: "GET",
+    dataType: "json",
+    url: "/networks/" + networkSlug,
+    success: function(data){
+      if (data.shows.length > 0) {
+        for (var i = 0; i < data.shows.length; i++) {
+          var show = data.shows[i];
+          var showTitle = show.title;
+          var showLocalLink = "/shows/" + show.slug;
+          var showDiv = $('<div/>');
+          showDiv.append("<a href='" + showLocalLink + "'><h2>" + showTitle + "</h2></a>");
+          $('.networkShows').append(showDiv);
+        }
+      } else {
+        $('.search-results').append("<h2>There were no networks matching your search. Please try again.</h2>");
+      }
+    }
+  });
+}
--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -4,22 +4,18 @@ $(document).ready(function() {
   showMore();
   showSearch();
   addShow();
-  loadShowShowPage();
   createNewUserShow();
-  loadShowsIndex();
-  loadNetworkShows();
-  // loadNetworks();  
+  
+
 });
 
-// $('.networks.index').ready(function() {
-  
-// });
-// $('.shows.index').ready(function(){
-  
-// });
-// $('.shows.show').ready(function(){
-  
-// });
+
+$('.shows.index').ready(function(){
+  loadShowsIndex();
+});
+$('.shows.show').ready(function(){
+  loadShowShowPage();
+});
 function ShowFromSearch(showData) {
       this.title = showData.name;
       this.url = showData.image.original;
@@ -203,29 +199,7 @@ function loadShowShowPage() {
     });
   }
 
-function loadNetworkShows() {
-  event.preventDefault();
-  var networkSlug = $('.networkShows').data("id");
-  $.ajax({
-    type: "GET",
-    dataType: "json",
-    url: "/networks/" + networkSlug,
-    success: function(data){
-      if (data.shows.length > 0) {
-        for (var i = 0; i < data.shows.length; i++) {
-          var show = data.shows[i];
-          var showTitle = show.title;
-          var showLocalLink = "/shows/" + show.slug;
-          var showDiv = $('<div/>');
-          showDiv.append("<a href='" + showLocalLink + "'><h2>" + showTitle + "</h2></a>");
-          $('.networkShows').append(showDiv);
-        }
-      } else {
-        $('.search-results').append("<h2>There were no networks matching your search. Please try again.</h2>");
-      }
-    }
-  });
-}
+
    function createNewUserShow() {
     $('form').submit(function(event) {
       $(this).hide();
