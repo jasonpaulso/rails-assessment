a8b0565f97dba4dd761038758b6b063b86bf8411
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-07-19 11:56:42 -0700

    debugged undefined errors in networks.js

--- app/assets/javascripts/networks.js
+++ app/assets/javascripts/networks.js
@@ -8,23 +8,25 @@ $('.networks.show').ready(function(){
 function loadNetworkShows() {
   event.preventDefault();
   var networkSlug = $('.networkShows').data("id");
-  $.ajax({
-    type: "GET",
-    dataType: "json",
-    url: "/networks/" + networkSlug,
-    success: function(data){
-      if (data.shows.length > 0) {
-        for (var i = 0; i < data.shows.length; i++) {
-          var show = data.shows[i];
-          var showTitle = show.title;
-          var showLocalLink = "/shows/" + show.slug;
-          var showDiv = $('<div/>');
-          showDiv.append("<a href='" + showLocalLink + "'><h2>" + showTitle + "</h2></a>");
-          $('.networkShows').append(showDiv);
+  if (networkSlug !== undefined) {
+    $.ajax({
+      type: "GET",
+      dataType: "json",
+      url: "/networks/" + networkSlug,
+      success: function(data){
+        if (data.shows.length > 0) {
+          for (var i = 0; i < data.shows.length; i++) {
+            var show = data.shows[i];
+            var showTitle = show.title;
+            var showLocalLink = "/shows/" + show.slug;
+            var showDiv = $('<div/>');
+            showDiv.append("<a href='" + showLocalLink + "'><h2>" + showTitle + "</h2></a>");
+            $('.networkShows').append(showDiv);
+          }
+        } else {
+          $('.search-results').append("<h2>There were no networks matching your search. Please try again.</h2>");
         }
-      } else {
-        $('.search-results').append("<h2>There were no networks matching your search. Please try again.</h2>");
       }
-    }
-  });
+    });
+  }
 }
