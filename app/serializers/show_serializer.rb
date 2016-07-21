0aa9915ecd7ccca7fc7ad6e9e95700279f15ee45
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-07-21 09:39:17 -0700

    added network to attributes in ShowSerializer

--- app/serializers/show_serializer.rb
+++ app/serializers/show_serializer.rb
@@ -1,5 +1,5 @@
 class ShowSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :url, :slug, :time, :day
+  attributes :id, :title, :description, :url, :slug, :time, :day, :network
   has_one :network, serializer: ShowNetworkSerializer
   has_many :users
 end
