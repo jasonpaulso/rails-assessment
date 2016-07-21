9ddd8fc77d39c82f43bb6a92b789c475bb59cd04
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-07-21 09:42:31 -0700

    removed unused UserShowSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :slug 
   has_many :shows
+  has_many :networks
 end
--- app/serializers/user_shows_serializer.rb
+++
@@ -1,3 +0,0 @@
-class UserShowsSerializer < ActiveModel::Serializer
-  attributes :id, :title, :network
-end
